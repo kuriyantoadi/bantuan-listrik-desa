0063e5cd25302715e0ff871842eb79b4742c81d0
kuriyantoadi <melatikuning212@gmail.com>
2025-02-21 07:43:59 +0700

    update

--- surveyor-penerima-lisdes/penerima_lisdes_modal_detail.php
+++ surveyor-penerima-lisdes/penerima_lisdes_modal_detail.php
@@ -74,12 +74,7 @@
                         <input type="text" class="form-control" value="<?= $d['jarak_rumah'] ?>" disabled>
                     </div>
                 </div>
-                <div class="row mb-4">
-                    <label class="col-md-3 form-label">Petugas Survei</label>
-                    <div class="col-md-9">
-                        <input type="text" class="form-control" value="<?= $d['petugas_survei'] ?>" disabled>
-                    </div>
-                </div>
+                
                 <div class="row mb-4">
                     <label class="col-md-3 form-label">Status Pemasangan</label>
                     <div class="col-md-9">
